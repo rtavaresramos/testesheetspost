342c0b066b8abaeeb9d2e6ff76cc1fe8e3363f2c
Rodrigo Tavares <rtavareskxd99@gmail.com>
2020-05-17 06:41:53 +0000

    starting a post method

---
+++ index.js
@@ -0,0 +1,5 @@
+const API_URL =   "https://spreadsheets.google.com/feeds/cells/1MAjPq2HiaBWbXXlHH6JSYa26twZ4VautqCOX6wxiUhY/1/public/full?alt=json"
+
+function insertData(e, form){
+    e.preventDefault()
+}
