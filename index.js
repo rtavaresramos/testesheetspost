68830b2efe326ebe5710aaa4eb8399f61c99b5a4
Rodrigo Tavares <rtavareskxd99@gmail.com>
2020-05-17 06:51:02 +0000

    starting a post method

--- index.js
+++ index.js
@@ -2,4 +2,38 @@ const API_URL =   "https://spreadsheets.google.com/feeds/cells/1MAjPq2HiaBWbXXlH
 
 function insertData(e, form){
     e.preventDefault()
+
+    const inputUsername = form.username
+    const username = inputUsername.value
+
+    const inputUseremail = form.useremail
+    const useremail = inputUseremail.value
 }
+
+
+// main.js 
+
+// POST request using fetch() 
+fetch(API_URL, { 
+	
+	// Adding method type 
+	method: "POST", 
+	
+	// Adding body or contents to send 
+	body: JSON.stringify({ 
+		name: username, 
+		email: useremail, 
+		userId: 1 
+	}), 
+	
+	// Adding headers to the request 
+	headers: { 
+		"Content-type": "application/json; charset=UTF-8"
+	} 
+}) 
+
+// Converting to JSON 
+.then(response => response.json()) 
+
+// Displaying results to console 
+.then(json => console.log(json)); 
